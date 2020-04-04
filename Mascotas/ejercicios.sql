27d86aec39d9e22c273683415417d9f1201ced40
Duvan sgf <damejia.98@hotmail.com>
2020-04-04 17:16:11 -0500

    subir

--- Mascotas/ejercicios.sql
+++ Mascotas/ejercicios.sql
@@ -4,3 +4,4 @@ Select especie, count(*) as Retirados from mascotas where estado = 'B' group by 
 SELECT ubicacion, count(*) as perros_macho from mascotas where estado = 'A' AND especie = 'P' AND SEXO = 'M' Group BY ubicacion;
 -- Distinct
 SELECT DISTINCT especie, ubicacion from mascotas Where estado = 'A';
+-- 
