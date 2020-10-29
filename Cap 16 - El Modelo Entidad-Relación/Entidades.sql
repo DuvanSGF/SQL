42c57097617255aeb92132c3f1e60d8b23c2b93f
Duvan sgf <damejia.98@hotmail.com>
2020-10-28 21:46:13 -0500

    Entidades Fuertes & Debiles

---
+++ Cap 16 - El Modelo Entidad-Relación/Entidades.sql
@@ -0,0 +1,13 @@
+/*
+Existen dos tipos de entidades, las fuertes, en ocasiones llamadas maestros, que
+de forma independiente identifican sus registros con una clave propia, y las débiles
+que dependen de una entidad fuerte para identificar sus registros, o si usted quiere,
+no tiene sentido su existencia sin una entidad fuerte donde apoyarse.
+
+En un modelo entidad-relación encontraremos esencialmente relaciones de dos
+tipos: 1 : N y, N : M. También encontraremos dos tipos de entidades: fuertes y
+débiles. Una entidad débil necesitará la clave de la entidad fuerte para identificar
+sus registros. La cardinalidad de una relación de una entidad débil con su maestro
+o entidad fuerte siempre será 1 : N.
+
+*/
