14b4909a7f02c5ee3566fee8a7dd8bca4fc57f78
Duvan sgf <damejia.98@hotmail.com>
2020-11-10 21:02:26 -0500

    Insert

---
+++ Cap 18 - INSERT , UPDATE, DELETE/Insert.sql
@@ -0,0 +1,10 @@
+/*
+La instrucción INSERT permite crear o insertar nuevos registros en una tabla,
+veamos su sintaxis con un ejemplo práctico: la inserción de un registro en la tabla
+ALUMNOS.
+*/
+
+insert into ALUMNOS
+(ID_ALUMNO , NOMBRE , APELLIDOS , F_NACIMIENTO)
+values
+(1 , 'Pablo' , 'Hernándaz Mata' , '1995-03-14') 
